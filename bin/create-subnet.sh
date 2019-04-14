989935c7efc68df2e0df0118ee18809d67f17b6e
Panayotis Matsinopoulos <>
2019-04-14 09:21:23 +0300

    Create subnet-1 using the VPC id output fromt he vpc stack

--- bin/create-subnet.sh
+++ bin/create-subnet.sh
@@ -9,6 +9,5 @@ aws cloudformation create-stack --stack-name ${STACK_NAME} --template-body file:
 --parameters \
 ParameterKey=Environment,ParameterValue=${ENVIRONMENT} \
 ParameterKey=SubnetName,ParameterValue=${SUBNET_NAME} \
-ParameterKey=VpcId,ParameterValue=${VPC_ID} \
 ParameterKey=AvailabilityZone,ParameterValue=${AVAILABILITY_ZONE} \
 ParameterKey=CidrBlock,ParameterValue=${CIDR_BLOCK}
