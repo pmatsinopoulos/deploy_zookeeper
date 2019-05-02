e98893b8a7e948d822321287a85ed5084a0dc792
Panayotis Matsinopoulos <>
2019-05-02 08:58:57 +0300

    Create the parent directory of the zookeeper::datalogstore

--- puppet/manifests/install_zookeeper.pp
+++ puppet/manifests/install_zookeeper.pp
@@ -1,6 +1,8 @@
-file { zookeeper_datalog_store:
+file { zookeeper_datalog_store_parent_directory_path:
+  # This is necessary, because the module fails to create the datalogstore folder
+  # if the parent directory does not exist.
   ensure => directory,
-  path => lookup("zookeeper::datalogstore", String)
+  path => dirname(lookup("zookeeper::datalogstore", String))
 }
 
 include zookeeper
