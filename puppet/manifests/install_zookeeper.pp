2f81467320baaf6942fce6e54c2d070b1b0eb38b
Panayotis Matsinopoulos <>
2019-05-02 09:29:58 +0300

    Added zkClient.sh in /usr/local/bin

--- puppet/manifests/install_zookeeper.pp
+++ puppet/manifests/install_zookeeper.pp
@@ -2,7 +2,13 @@ file { zookeeper_datalog_store_parent_directory_path:
   # This is necessary, because the module fails to create the datalogstore folder
   # if the parent directory does not exist.
   ensure => directory,
-  path => dirname(lookup("zookeeper::datalogstore", String))
+  path   => dirname(lookup("zookeeper::datalogstore", String))
 }
 
 include zookeeper
+
+file { zookeeper_client_in_path:
+  ensure => link,
+  path   => '/usr/local/bin/zkClient.sh',
+  target => '/usr/share/zookeeper/bin/zkClient.sh',
+}
