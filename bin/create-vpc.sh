03810c599dd8c806bf480f6d1d58f9276787c732
Panayotis Matsinopoulos <>
2019-05-04 19:17:23 +0300

    Creates a Route 53 Hosted zone when creating the VPC

--- bin/create-vpc.sh
+++ bin/create-vpc.sh
@@ -10,6 +10,7 @@ source ${CONFIGURATION_FILE}
 aws cloudformation create-stack --stack-name ${VPC_STACK_NAME} --template-body file://${VPC_CLOUD_FORMATION_FILE} \
 --parameters \
 ParameterKey=Environment,ParameterValue=${ENVIRONMENT} \
-ParameterKey=Ec2LaunchTemplateName,ParameterValue=${EC2_LAUNCH_TEMPLATE_NAME}
+ParameterKey=Ec2LaunchTemplateName,ParameterValue=${EC2_LAUNCH_TEMPLATE_NAME} \
+ParameterKey=Region,ParameterValue=${AWS_DEFAULT_REGION}
 
 aws cloudformation wait stack-create-complete --stack-name ${VPC_STACK_NAME}
