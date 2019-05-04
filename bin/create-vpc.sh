56d2a1a45355c0d60be783a62b0a4d7ed3b60bf7
Panayotis Matsinopoulos <>
2019-05-04 19:50:36 +0300

    Use AWS::Region instead of giving the region as parameter

--- bin/create-vpc.sh
+++ bin/create-vpc.sh
@@ -10,7 +10,6 @@ source ${CONFIGURATION_FILE}
 aws cloudformation create-stack --stack-name ${VPC_STACK_NAME} --template-body file://${VPC_CLOUD_FORMATION_FILE} \
 --parameters \
 ParameterKey=Environment,ParameterValue=${ENVIRONMENT} \
-ParameterKey=Ec2LaunchTemplateName,ParameterValue=${EC2_LAUNCH_TEMPLATE_NAME} \
-ParameterKey=Region,ParameterValue=${AWS_DEFAULT_REGION}
+ParameterKey=Ec2LaunchTemplateName,ParameterValue=${EC2_LAUNCH_TEMPLATE_NAME}
 
 aws cloudformation wait stack-create-complete --stack-name ${VPC_STACK_NAME}
