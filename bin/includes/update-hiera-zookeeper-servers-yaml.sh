8e5f186edd45c505066c053330a62933f16aa402
Panayotis Matsinopoulos <>
2019-05-11 22:13:06 +0300

    Added more echo commands to make sure that we loop correctly for the additions of zookeeper nodes to servers yaml file

--- bin/includes/update-hiera-zookeeper-servers-yaml.sh
+++ bin/includes/update-hiera-zookeeper-servers-yaml.sh
@@ -1,5 +1,7 @@
 update_hiera_zookeeper_servers_yaml() {
     local NUMBER_OF_SERVERS=$(ls -l ${HIERA_DIRECTORY_WITH_ZOOKEEPER_HOSTS_DATA} | wc -l)
+    echo "NUMBER_OF_SERVER found ${NUMBER_OF_SERVERS}"
+
     local NAME_FOR_NODE=""
 
     cat << EOF > ${SERVERS_FILE}
@@ -11,6 +13,8 @@ EOF
 
       NAME_FOR_NODE="${ZOOKEEPER_NODE_STACK_NAME}-${i}"
       NAME_FOR_NODE=$(echo ${NAME_FOR_NODE} | tr [:upper:] [:lower:]).${DOMAIN_NAME}
+
+      echo "(index: ${i}): About to add the node ${NODE_FOR_NAME} into ${SERVERS_FILE}..."
 
       cat << EOF >> ${SERVERS_FILE}
   - '${NAME_FOR_NODE}'
