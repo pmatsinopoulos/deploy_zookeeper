23caf38c16015c6f537d6e7b741453fe20bbb81b
Panayotis Matsinopoulos <>
2019-05-12 08:52:51 +0300

    Fixed the way we add the servers into zookeeper server.yaml

--- bin/includes/update-hiera-zookeeper-servers-yaml.sh
+++ bin/includes/update-hiera-zookeeper-servers-yaml.sh
@@ -3,16 +3,20 @@ update_hiera_zookeeper_servers_yaml() {
     local NAME_FOR_NODE=""
     local EXTRACTED_NODE_INTEGER=0
 
-    read -r -a SERVERS_FILES_ARRAY <<< $(ls ${HIERA_DIRECTORY_WITH_ZOOKEEPER_HOSTS_DATA})
+    SERVERS_FILES_ARRAY=("${HIERA_DIRECTORY_WITH_ZOOKEEPER_HOSTS_DATA}"/*)
 
     cat << EOF > ${SERVERS_FILE}
 ---
 zookeeper::servers:
 EOF
+    echo "Array: ${SERVERS_FILES_ARRAY[*]}..."
 
     for item in ${SERVERS_FILES_ARRAY[*]}; do
 
+      # remove the yaml file extension
       NAME_FOR_NODE=${item%.yaml}
+      # remove the directory path and keep the file name
+      NAME_FOR_NODE=$(echo ${NAME_FOR_NODE##*/})
 
       EXTRACTED_NODE_INTEGER=$(echo ${NAME_FOR_NODE} | cut -d '-' -f2 | cut -d '.' -f 1)
 
@@ -21,7 +25,6 @@ EOF
       cat << EOF >> ${SERVERS_FILE}
   '${EXTRACTED_NODE_INTEGER}': '${NAME_FOR_NODE}'
 EOF
-
+      echo "item at end of for loop: ${item}..."
     done
-
 }
